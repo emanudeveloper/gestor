ccc418124a9f4e3449eb9aa87c86679a0cb565a0
Emanuel Bellido <emanudeveloper@gmail.com>
2022-10-10 10:47:58 -0500

    push to Heroku 10/10/2022  10:47am

--- controladores/recientesController.js
+++ controladores/recientesController.js
@@ -3,6 +3,9 @@ const reciente = {}
 
 reciente.mostrarVista =  async (req, res)=>{
     const documentos = await documentoModel.find({});
+    // console.log(documentos);
+    // console.log(documentos[3]);
+    // console.log(documentos[3].autor);
     // documentos = JSON.stringify(documentos)
     // res.render('recientes.pug', {documentos, numElementos:documentos.length});
     res.render('recientes.pug', {documentos, tamanio: documentos.length});
