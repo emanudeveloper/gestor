51cab93c8a6e7d965f3126a50e681755aed07e99
Emanuel Bellido <emanudeveloper@gmail.com>
2022-10-18 09:32:34 -0500

    diresa multer enviar pdf y mostrar en reciente  18/10/2022 09:32am

--- controladores/registrarController.js
+++ controladores/registrarController.js
@@ -1,56 +1,72 @@
 const documento = require("../modelos/documentoModel");
 const path = require('path');
 const multer = require("multer");
+const registrarController = {}
+
 const storage = multer.diskStorage({
     destination:function(req, file ,cb){
-        cb(null, 'documentos');    
+        
+        cb(null, path.join('documentos', 'temporal'));    
     },
     filename:function(req, file, cb){
         console.log(file);
-        cb(null, `${file.originalname}`);
+        // let nombreArchivo = (new Date).toISOString().slice(0, 10);
+        // console.log(nombreArchivo);
+        // nombreArchivo = nombreArchivo.concat("-", file.originalname);
+        // console.log(nombreArchivo);
+        // cb(null, nombreArchivo);
+        // cb(null, `${Date.now()}-${file.originalname}`);
+        cb(null, file.originalname);
+        
     }
     });
 
     const docMulter = multer({storage:storage});
-    exports.documentoSingle=docMulter.single('pdf')
-// const { send } = require("process");
-const registrarController = {}
+
+registrarController.documentoSingle=docMulter.single('pdf')
+
 
 registrarController.mostrarVista = (req, res)=>{
 
     res.render("registrar.pug");
 }
 
-registrarController.registrarDocumento = (req, res)=>{
+registrarController.registrarDocumento =  (req, res)=>{
     
-    // let nuevoDocumentoModel = new documento(req.body);
+    let nuevoDocumentoModel = new documento(req.body);
     
-    // const {oficina, f_doc, pdf} = req.body;
-    // const anio=f_doc.substring(0,4);
-    // const mes = f_doc.substring(5,7);
-    // const ruta = path.join(oficina, anio, mes, pdf);
-    // nuevoDocumentoModel.url = ruta;
+    // const {oficina, f_doc, pdf} =  req.body;
+    const {oficina, f_doc} =  req.body;
+    // console.log(req.body.f_doc);
+    const anio=f_doc.substring(0,4);
+    const mes = f_doc.substring(5,7);
+    const dia = f_doc.substring(8,10);
+    // console.log(oficina, anio, mes, dia, req.file.filename);
+    // const ruta = path.join(oficina, anio, mes, dia, req.file.filename);
+    // const ruta = path.join("documentos", "temporal", req.file.filename);
+    const ruta = path.join("documentos", "temporal", req.file.filename);
+    console.log(ruta)
+    nuevoDocumentoModel.url = ruta;
     
-    // nuevoDocumentoModel
-    //     .save(nuevoDocumentoModel)
-    //     .then(data=>{
-    //         console.log(data);
-    //         res.render('registrar.pug');
-    //     })
-    //     .catch(err=>{
-    //         res.status(500).send({
-    //             message:
-    //               err.message || "No se pudo registrar el documento"
-    //     })});
+    nuevoDocumentoModel
+        .save(nuevoDocumentoModel)
+        .then(data=>{
+            console.log(data);
+            res.render('registrar.pug');
+        })
+        .catch(err=>{
+            res.status(500).send({
+                message:
+                  err.message || "No se pudo registrar el documento"
+        })});
 
-    // console.log(req.file);
     // const files = req.file;
 
 
     
     //{dest: 'documentos/' }
 
-    res.send(req.file);
+    // res.send(req.file);
 }
 
-exports = registrarController;
+module.exports = registrarController;
