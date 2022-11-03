1347d66d6d5f13ee364f02afebf03bb9d36b9a1e
Emanuel Bellido <emanudeveloper@gmail.com>
2022-11-03 10:06:30 -0500

    conectando mongodb atlas heroku

--- conexiondb.js
+++ conexiondb.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose'); //var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mesapartes"; 
-// var url = "mongodb+srv://diresa-mesa:mA0fORQB59l5bgOk@mesadepartes.qqsmout.mongodb.net/mesapartes?retryWrites=true&w=majority";
+// var url = "mongodb://localhost:27017/mesapartes"; 
+var url = "mongodb+srv://diresa-mesa:mA0fORQB59l5bgOk@mesadepartes.qqsmout.mongodb.net/mesapartes?retryWrites=true&w=majority";
 
 //diresa-mesa
 //mA0fORQB59l5bgOk
 
 module.exports = mongoose.connect((url || process.env.MONGODB_URI), {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useFindAndModify:flase
 
 }).then((ans)=>{  
   console.log(`Conectado`);
