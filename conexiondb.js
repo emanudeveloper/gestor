6c1c0f7b146a3060dff58e9abb9c40f705923938
Emanuel Bellido <emanudeveloper@gmail.com>
2022-11-03 09:42:28 -0500

    desactivando morgan

--- conexiondb.js
+++ conexiondb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); //var MongoClient = require('mongodb').MongoClient;
-// var url = "mongodb://localhost:27017/mesapartes"; 
-var url = "mongodb+srv://diresa-mesa:mA0fORQB59l5bgOk@mesadepartes.qqsmout.mongodb.net/mesapartes?retryWrites=true&w=majority";
+var url = "mongodb://localhost:27017/mesapartes"; 
+// var url = "mongodb+srv://diresa-mesa:mA0fORQB59l5bgOk@mesadepartes.qqsmout.mongodb.net/mesapartes?retryWrites=true&w=majority";
 
 //diresa-mesa
 //mA0fORQB59l5bgOk
