63288bf87c5bb2e7547d0bf37e6e2e6b3ff96e5f
Emanuel Bellido <emanudeveloper@gmail.com>
2022-12-01 09:40:01 -0500

    push trabajo: abriendo carpetas

--- public/js/script.js
+++ public/js/script.js
@@ -20,7 +20,7 @@ function mostrarArchivos(archivos, carpetas){
         
         console.log(el);
         nuevaCarpeta=null;
-        nuevaCarpeta = Object.assign(document.createElement('div'), { className: 'tarjeta'});
+        nuevaCarpeta = Object.assign(document.createElement('div'), { className: 'tarjetaPDF'});
         // nuevaCarpeta.setAttribute("contenteditable","true");
         nuevaCarpeta.appendChild(document.createTextNode(`${tamanio+1} ${el}`));
         
@@ -68,7 +68,7 @@ function agregar(id){
     // var nuevaCarpeta = Object.assign(document.createElement('a'), { className: 'tarjeta' });
     var nuevaCarpeta = Object.assign(document.createElement('a'), { className: 'tarjeta'});
     // nuevaCarpeta.setAttribute("contenteditable","true");
-    
+
     // nuevaCarpeta.classList.add("nuevaCarpeta");    
     // nuevaCarpeta.appendChild(document.createTextNode('Nueva carpeta.'));
     
@@ -252,76 +252,6 @@ input.addEventListener('change',()=>{
         console.log("error: ", e);
     }        
 });
-
-
-
-// var directorio = document.getElementById('tarjetas');
-// var directorios = ["Asesoría Jurídica", "Logística", "Servicios de Salud",
-//                     "Administración", "Inteligencia Sanitaria", "Promoción de la Salud",
-//                     "Contro Institucional", "Estadística e Informática", "Laboratorio Referencial R."]
-// let html;
-// let idNum="";
-// let arreglo=[];
-
-
-//  function insertarDirectorio(){
-    
-//     html="";
-//     idNum=""; 
-//     directorios.forEach(function callback(carpeta, index, array) {
-                
-//         idNum = "carp".concat(`${index}`);
-//         html += `<a id=${idNum} href="" onclick=abrirCarpeta(this.id) class="tarjeta">${index+1}  ${carpeta} </a>`;
-//     });
-//     directorio.innerHTML = html;
-// }
-
-    
-// function abrirCarpeta(id){
-    // arreglo.forEach(function (value,index, array){        
-    //     // doc.innerHTML="<p>Hola mundo</p>";
-    // })
-    // carpeta.style="backgroundColor:red;";
-    // id.getAttribute('id');
-    //id.getAttribute('id')
-    // var doc = document.getElementById(id);
-    // alert(id);
-    // doc.innerHTML="<p>Hola mundo</p>"; 
-    // doc.style="color:black";
-// }
-//insertarDirectorio();
-
-// arreglo.push(idNum);
-        // html += `<a id=${idNum} href="/papelera" onclick=abrirCarpeta(this.id)><div class="tarjeta">${index+1}  ${carpeta} </div></a>`;
-
-
-    // directorio.appendChild(`<div class="tarjeta">${directorio}</div>`;     <a id="carp${index+1}"> </a>
-    // directorio.innerHTML = html;
-    // console.log("directorio: ", directorio.firstChild.nodeName);
-    // console.log("directorio2: ", directorio.children.length);
-    // obtenerDirectorios();        
-
-
-
-
-// const  
-// function seleccionar(){
-
-//     // menuArchivos.style.backgroundColor="white";
-//     // archivos.style.backgroundColor = "white";
-//     menuArchivos.style.backgroundColor ="white";
-//     archivos.style.backgroundColor = "white";
-//     menuArchivos.firstChild.style.color= "black";
-//     // html = 
-// }
-
-
-// async function obtenerDirectorios(){
-
-//     let archivo = await window.FileSystemDirectoryReader.readEntries(()=>{})); 
-//     console.log(archivo);
-// }
-
 
 
 
