eadff1a2be19d4ac41d7809edfb0265539dcd8d5
Emanuper <emanudeveloper@gmail.com>
2024-06-04 23:06:25 -0500

    Se configuro la base de datos de mongodb atlas con la app

--- conexiondb.js
+++ conexiondb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose'); //var MongoClient = require('mongodb').MongoClient;
 // var url = "mongodb://localhost:27017/mesapartes"; 
 // var url = "mongodb+srv://diresa-mesa:mA0fORQB59l5bgOk@mesadepartes.qqsmout.mongodb.net/mesapartes?retryWrites=true&w=majority";
-var url = "mongodb+srv://diresa-mesa:iXJd2kSqzUE68dEv@clusteralmacen.eldbjzy.mongodb.net/?retryWrites=true&w=majority&appName=clusteralmacen";
+var url = "mongodb+srv://diresa-mesa:iXJd2kSqzUE68dEv@clusteralmacen.eldbjzy.mongodb.net/?retryWrites=true&w=majority&appName=clusteralmacen&connectTimeoutMS=30000";
 //iXJd2kSqzUE68dEv  ultima pasword
 //diresa-mesa
 
